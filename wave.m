43320b6a4788761bd63c9800540cb1cca884e387
JIankui Feng <37136232+fengjiankui121@users.noreply.github.com>
2018-03-24 20:59:10 +0800

    Update wave.m

--- wave.m
+++ wave.m
@@ -8,6 +8,6 @@ newdata=idwt(cA,[],'haar');%获得尺度系数的信号
 newdata=idwt([],cD,'haar');%获得细节系数的信号
 %--------分解多层-----
 [c,l]=wavedec(data,3,'haar');
-newdata=waverec(c,l,'haar')
+newdata=waverec(c,l,'haar');%重构
 newdata=wrcoef('a',c,l,3);%计算第3层的尺度系数的信号
 newdata=wrcoef('d',c,l,3);%计算第3层的细节系数的信号
