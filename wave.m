5ece97186240f69f88d9e4201bb2455e64e36e8c
fengjiankui121 <fengjiankui121@163.com>
2018-03-24 20:09:32 +0800

    小波
    小波初学

---
+++ wave.m
@@ -0,0 +1,12 @@
+%----------小波变换--------
+%-----分解1层---------
+%----信号data--
+[cA,cD]=dwt(data,'haar');   %cA:尺度系数，cD细节系数
+%重构
+newdata=idwt(cA,cD,'haar');
+newdata=idwt(cA,[],'haar');%获得尺度系数的信号
+newdata=idwt([],cD,'haar');%获得细节系数的信号
+%--------分解多层-----
+[c,l]=wavedec(data,3,'haar');
+newdata=wrcoef('a',c,l,3);%计算第3层的尺度系数的信号
+newdata=wrcoef('d',c,l,3);%计算第3层的细节系数的信号
